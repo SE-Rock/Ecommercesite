53d0e7f2391b097b0e1577f3e15bae664be56426
Dhanya <dhan@dhanya.com>
2023-04-01 23:50:08 +0530

    first commit

---
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Category;
+use App\Models\Subcategory;
+use Livewire\Component;
+
+class AdminCategoryComponent extends Component
+{
+    public function deleteCategory($id){
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message','Category has been deleted successfully');
+    }
+
+    public function deleteSubcategory($id){
+        $scategory = Subcategory::find($id);
+        $scategory->delete();
+        session()->flash('message','Subcategory has been deleted successfully!');
+    }
+    public function render()
+    {
+        $categories = Category::paginate(5);
+        return view('livewire.admin.admin-category-component',['categories'=>$categories])->layout('layouts.base');
+    }
+}
